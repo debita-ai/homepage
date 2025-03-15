df283fe5041419bae7dd2bb020a794eb8fc2e612
p-lacerda <polivei@cowboymail.okstate.edu>
2025-03-14 22:01:58 -0300

    feat: fix ref

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "@typescript-eslint/no-explicit-any": "off"
     },
   },
 ];
